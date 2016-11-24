7a8b67fd8c32b799432548eb423e2a0df47ae17c
Michyo Song <michyosyn@gmail.com>
2016-11-24 13:27:20 +0800

    Initial Commit

---
+++ SwiftyRest/SwiftyRest.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyRest.h
+//  SwiftyRest
+//
+//  Created by Song, Michyo on 11/24/16.
+//  Copyright © 2016 Song, Michyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyRest.
+FOUNDATION_EXPORT double SwiftyRestVersionNumber;
+
+//! Project version string for SwiftyRest.
+FOUNDATION_EXPORT const unsigned char SwiftyRestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyRest/PublicHeader.h>
+
+
